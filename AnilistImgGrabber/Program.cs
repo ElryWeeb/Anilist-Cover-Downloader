e6e4e204af24867d182ee5699c6e0601b03b6257
ElryWeeb <144011449+ElryWeeb@users.noreply.github.com>
2024-03-19 22:56:30 +0100

    Massive Rewrite

--- AnilistImgGrabber/Program.cs
+++ AnilistImgGrabber/Program.cs
@@ -1,9 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Diagnostics;
-using AniListNet; // Contains base classes for interacting with the API
-using AniListNet.Parameters; // Contains classes for mutation and filtering
-using AniListNet.Objects; // Contains model classes for handling data
+using Anilist4Net;
+using Anilist4Net.Enums;
 
 Console.WriteLine("Checking for Manga.txt");
 
@@ -20,78 +19,122 @@ if (!Directory.Exists("Output"))
     Directory.CreateDirectory("Output");
 }
 
-var lines = File.ReadLines("Manga.txt");
-var client = new AniClient();
-Process process = new Process();
+var lines = File.ReadAllLines("Manga.txt");
+var client = new Client();
+List<string> recheck = new List<string>();
+
 foreach (var line in lines)
 {
-    Console.WriteLine("Scanning for: " + line);
-    var results = await client.SearchMediaAsync(new SearchMediaFilter
-    {
-        
-        Query = line, // The term to search for
-        Type = MediaType.Manga, // Filters search results to anime only
-        Sort = MediaSort.Popularity // Sorts them by popularity
-    });
-
-    var result = results.Data.First();
     
-    string imageUrl = result.Cover.ExtraLargeImageUrl.ToString();
+    Console.WriteLine("Scanning for: " + line);
+    tryagain:
+    var results = await client.GetMediaBySearch(line, MediaTypes.MANGA, 1, 10);
+    
+    if (results == null)
+    {
+        Console.WriteLine("Rate-limited. Waiting 1 Minute.");
+        Thread.Sleep(61000);
+        goto tryagain;
+    }
+    if (results.PageInfo.Total < 1)
+    {
+        Console.WriteLine("Couldnt find " + line + " on Anilist.");
+        Console.WriteLine("Adding Manga to NotFound.txt");
+        if (!File.Exists("NotFound.txt"))
+        {
+            File.Create("NotFound.txt").Close();
+        }
+        File.AppendAllText("NotFound.txt",line + Environment.NewLine);
+    }
+
+    var result = results.Media.First();
+
+    string imageUrl = result.CoverImageExtraLarge;
     string saveFile = line.Replace("'", "");
     saveFile = saveFile.Replace(".", "-");
     string fileType = imageUrl.Split('.').Last();
     
-    if (line == result.Title.EnglishTitle || line == result.Title.RomajiTitle)
+    if (line == result.EnglishTitle || line == result.RomajiTitle)
     {
         //Download
         using (var webClient = new HttpClient())
         {
-            using (var s = webClient.GetStreamAsync(imageUrl))
+            webClient.Timeout = TimeSpan.FromSeconds(5);
+            using (var s = webClient.GetStreamAsync(imageUrl).Result)
             {
                 using (var fs = new FileStream("Output\\" + saveFile + "." + fileType, FileMode.OpenOrCreate))
                 {
-                    s.Result.CopyTo(fs);
+                    s.CopyTo(fs);
                 }
+
+            }
+        }
+        
+    }
+    else
+    {
+        Console.WriteLine($"We couldn't confirm the result for {line}, adding for a later Re-Check.");
+        recheck.Add(line);
+    }
+
+    Thread.Sleep(1000); //So we don't get rate-limited
+}
+
+Process process = new Process();
+foreach (var check in recheck)
+{
+    Console.WriteLine("Please review all Manga that were not 100% Identifiable.");
+    tryagain:
+    var results = await client.GetMediaBySearch(check, MediaTypes.MANGA, 1, 10);
+    
+    if (results == null)
+    {
+        Console.WriteLine("Rate-limited. Waiting 1 Minute.");
+        Thread.Sleep(61000);
+        goto tryagain;
+    }
+
+    var result = results.Media.First();
+
+    string imageUrl = result.CoverImageExtraLarge;
+    string saveFile = check.Replace("'", "");
+    saveFile = saveFile.Replace(".", "-");
+    string fileType = imageUrl.Split('.').Last();
+    process.StartInfo.UseShellExecute = true;
+    process.StartInfo.FileName = "chrome";
+    process.StartInfo.Arguments = @result.SiteUrl; 
+    process.Start();
+    Console.WriteLine("Please check your Browser if this is result is correct.");
+    Console.WriteLine("Press Y if it is, any other if not.");
+    var answer = Console.ReadKey(true);
+
+    if (answer.Key == ConsoleKey.Y)
+    {
+        //Download
+        using (var webClient = new HttpClient())
+        {
+            webClient.Timeout = TimeSpan.FromSeconds(5);
+            using (var s = webClient.GetStreamAsync(imageUrl).Result)
+            {
+                using (var fs = new FileStream("Output\\" + saveFile + "." + fileType, FileMode.OpenOrCreate))
+                {
+                    s.CopyTo(fs);
+                }
+
             }
         }
     }
     else
     {
-        Console.WriteLine($"We couldn't confirm the result for {line}");
-        process.StartInfo.UseShellExecute = true;
-        process.StartInfo.FileName = "chrome";
-        process.StartInfo.Arguments = @result.Url.AbsoluteUri; 
-        process.Start();
-        Console.WriteLine("Please check your Browser if this is result is correct.");
-        Console.WriteLine("Press Y if it is, any other if not.");
-        var answer = Console.ReadKey(true);
-
-        if (answer.Key == ConsoleKey.Y)
+        Console.WriteLine("Adding Manga to NotFound.txt");
+        if (!File.Exists("NotFound.txt"))
         {
-            //Download
-            using (var webClient = new HttpClient())
-            {
-                using (var s = webClient.GetStreamAsync(imageUrl))
-                {
-                    using (var fs = new FileStream("Output\\" + saveFile + "." + fileType, FileMode.OpenOrCreate))
-                    {
-                        s.Result.CopyTo(fs);
-                    }
-                }
-            }
+            File.Create("NotFound.txt").Close();
         }
-        else
-        {
-            Console.WriteLine("Adding Manga to notfound.txt");
-            if (!File.Exists("notfound.txt"))
-            {
-                File.Create("notfound.txt").Close();
-            }
-            File.AppendAllText("notfound.txt",line);
-        }
+        File.AppendAllText("NotFound.txt",check + Environment.NewLine);
     }
-    Thread.Sleep(750); //So we dont get rate-limited
 }
+
 
 Console.WriteLine("Done, press any Key to exit.");
 Console.ReadKey(true);
